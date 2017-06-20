f2665a08e3dca82c22538d579db06a0d1999c6bf
talr4 <tal.rosler@gmail.com>
2017-06-20 22:26:39 +0300

    a

--- RANSAC_Wrapper.m
+++ RANSAC_Wrapper.m
@@ -3,15 +3,15 @@ function [H_ransac] = RANSAC_Wrapper(matches, fittingfn, distfn, degenfn, s, t, 
 p1(:,i) = [matches(1, i) matches(2, i) 1];
 p2(:,i) = [matches(3, i) matches(4, i) 1];
 
-[p1, T1] = normalizePoints(p1);
-[p2, T2] = normalizePoints(p2);
+%[p1, T1] = normalizePoints(p1);
+%[p2, T2] = normalizePoints(p2);
 
 [H_ransac, inliers] = ransac([x1; x2], fittingfn, distfn, degenfn, s, t, feedback, ...
                                maxDataTrials, maxTrials);
 
-H_ransac = fittingfn(p1(:,inliers), p2(:,inliers));
+%H_ransac = fittingfn(p1(:,inliers), p2(:,inliers));
 
-H_ransac = T2\H*T1;    
+%H_ransac = T2\H*T1;    
 
 
                            
