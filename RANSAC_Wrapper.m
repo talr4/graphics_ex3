a355a9450873eef9c539e7ab8a461d8fb5c00598
Liron Moshkovich <lironmosh@gmail.com>
2017-06-23 13:09:30 +0300

    RANSAC

--- RANSAC_Wrapper.m
+++ RANSAC_Wrapper.m
@@ -1,22 +1,18 @@
-function [H_ransac] = RANSAC_Wrapper(matches, fittingfn, distfn, degenfn, s, t, feedback, maxDataTrials, maxTrials)
-
+function [H, inliers] = ransacfithomography(matches, fittingfn, distfn, degenfn, s, t, feedback, maxDataTrials, maxTrials)
+        
     numberOfPoints = size(matches, 2);
-
     for i = 1 : numberOfPoints
       x1(:,i) = [matches(1, i) matches(2, i) 1];
       x2(:,i) = [matches(3, i) matches(4, i) 1];
     end
 
-    [x1, T1] = normalise2dpts(x1);
-    [x2, T2] = normalise2dpts(x2);
+    [x1, T1] = normalizePoints(x1);
+    [x2, T2] = normalizePoints(x2);
 
-[H_ransac, inliers] = ransac([x1; x2], fittingfn, distfn, degenfn, s, t);
-
-    H = fittingfn(x1(:,inliers), x2(:,inliers));
+    [H, inliers] = ransac([x1; x2], fittingfn, distfn, degenfn, s, t, feedback, maxDataTrials, maxTrials);
+    
+    H = homography2d(x1(:,inliers), x2(:,inliers));
+    
     % Denormalise
-    H = T2\H*T1;  
-
-
-                           
-end            
-                        
+    H = T2\H*T1;    
+    
