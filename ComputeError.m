69fc50b1b17fb00baf6ceb0961fa00d340c37f1d
Liron Moshkovich <lironmosh@gmail.com>
2017-06-23 11:59:09 +0300

    Error

--- ComputeError.m
+++ ComputeError.m
@@ -3,12 +3,12 @@ error = 0;
 n = size(pnts_gt);
 for i = 1 : n
     %un-homegonize the points
-    x1 = pnts_gt(i, 1)/pnts_gt(i, 3);
-    y1 = pnts_gt(i, 2)/pnts_gt(i, 3);
-    x2 = pnts_computed(i,1)/pnts_computed(i,3);
-    y2 = pnts_computed(i,2)/pnts_computed(i,3);
+    x1 = pnts_gt(1, i)/pnts_gt(3, i);
+    y1 = pnts_gt(2, i)/pnts_gt(3, i);
+    x2 = pnts_computed(1,i)/pnts_computed(3, i);
+    y2 = pnts_computed(2,i)/pnts_computed(3, i);
     
-    distace = abs(sqrt((x2-x1)^2+(y2-y1)^2));
+    distace = sqrt((x2-x1)^2+(y2-y1)^2);
     
     error = error + distace;
 end
